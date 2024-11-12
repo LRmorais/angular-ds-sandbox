3d632b928fd4d69ed296055f854b72111bb0ad7b
Lucas Morais <lucas.morais23ti@gmail.com>
2024-11-12 19:36:49 -0300

    feat: criando tela para agrupar tipos de implementação dos inputs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,19 @@ import { Routes } from '@angular/router';
 import { DefaultLayoutComponent } from './layout/default/default.component';
 
 export const routes: Routes = [
-  { 
+  {
     path: '',
-    component: DefaultLayoutComponent
+    component: DefaultLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'inputs',
+        pathMatch: 'full',
+      },
+      {
+        path: 'inputs',
+        loadComponent: () => import('./pages/inputs/inputs.component').then(m => m.InputsComponent),
+      }
+    ]
   },
 ];
