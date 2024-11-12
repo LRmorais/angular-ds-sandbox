013e463f5f613ce007ae84bf4d9f74e39c2fc133
Lucas Morais <lucas.morais23ti@gmail.com>
2024-11-11 22:31:16 -0300

    estruturando o menu de usuario

--- src/app/layout/default/components/menu-usuario/menu-usuario.component.ts
+++ src/app/layout/default/components/menu-usuario/menu-usuario.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Component, input, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-usuario',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatDividerModule
+  ],
   templateUrl: './menu-usuario.component.html',
-  styleUrl: './menu-usuario.component.scss'
+  styleUrls: ['./menu-usuario.component.scss']
 })
-export class MenuUsuarioComponent {
+export class MenuUsuarioComponent implements OnInit, OnDestroy {
+  public showAvatar = input(false);
 
+  constructor(private _router: Router) {}
+
+  get username(): string | null {
+    throw new Error('Method not implemented.');
+  }
+
+  get profile(): string | null {
+    throw new Error('Method not implemented.');
+  }
+
+  public ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  signOut(): void {
+    throw new Error('Method not implemented.');
+  }
 }
