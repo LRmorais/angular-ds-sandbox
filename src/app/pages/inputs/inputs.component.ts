9af79a6f43a92a80623312616230156fe16682c4
Lucas Morais <lucas.morais23ti@gmail.com>
2024-11-14 18:34:59 -0300

    feat: aprendendo sobre o host

--- src/app/pages/inputs/inputs.component.ts
+++ src/app/pages/inputs/inputs.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {InputAntigoComponent} from "../../shared/components/input/input-antigo/input-antigo.component";
 import {InputSignalComponent} from "../../shared/components/input/input-signal/input-signal.component";
+import {HostLearningComponent} from "../../shared/components/host-learning/host-learning.component";
 
 @Component({
   selector: 'inputs',
   standalone: true,
-  imports: [InputAntigoComponent, InputSignalComponent],
+  imports: [InputAntigoComponent, InputSignalComponent, HostLearningComponent],
   templateUrl: './inputs.component.html',
   styleUrl: './inputs.component.scss'
 })
